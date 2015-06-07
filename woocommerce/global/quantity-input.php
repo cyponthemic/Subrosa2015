44f1a18056fdda4951d5a9c78c3f09598c6b7624
Alex <Chavet>
2015-06-07 13:20:32 +1000

    Quantity fix

--- woocommerce/global/quantity-input.php
+++ woocommerce/global/quantity-input.php
@@ -13,4 +13,4 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 ?>
 <span class="small-3 qty-label columns">Quantity</span>
-<div class="quantity small-3 columns"><input type="number" step="6" <?php if ( is_numeric( $min_value ) ) : ?>min="6"<?php endif; ?> <?php if ( is_numeric( $max_value ) ) : ?>max="<?php echo esc_attr( $max_value ); ?>"<?php endif; ?> name="<?php echo esc_attr( $input_name ); ?>" value="<?php echo esc_attr( $input_value ); ?>" title="<?php _ex( 'Qty', 'Product quantity input tooltip', 'woocommerce' ) ?>" class="input-text qty text" size="4" /></div>
+<div class="quantity small-3 columns"><input type="number" step="1" <?php if ( is_numeric( $min_value ) ) : ?>min="1"<?php endif; ?> <?php if ( is_numeric( $max_value ) ) : ?>max="<?php echo esc_attr( $max_value ); ?>"<?php endif; ?> name="<?php echo esc_attr( $input_name ); ?>" value="<?php echo esc_attr( $input_value ); ?>" title="<?php _ex( 'Qty', 'Product quantity input tooltip', 'woocommerce' ) ?>" class="input-text qty text" size="4" /></div>
