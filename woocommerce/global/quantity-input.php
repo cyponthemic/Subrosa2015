9be20ad0a883b2f9bec699e754d8ea1052dc183c
Alex <Chavet>
2016-02-16 18:50:56 +1100

    Revert "Delete test"
    This reverts commit adfd7090da27cd179b6871b32edce88fc9f30f95.

---
+++ woocommerce/global/quantity-input.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Product quantity inputs
+ *
+ * @author 		WooThemes
+ * @package 	WooCommerce/Templates
+ * @version     2.1.0
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly
+}
+
+?>
+<span class="small-3 qty-label columns">Quantity</span>
+<div class="quantity small-3 columns"><input type="number" step="1" <?php if ( is_numeric( $min_value ) ) : ?>min="1"<?php endif; ?> <?php if ( is_numeric( $max_value ) ) : ?>max="<?php echo esc_attr( $max_value ); ?>"<?php endif; ?> name="<?php echo esc_attr( $input_name ); ?>" value="<?php echo esc_attr( $input_value ); ?>" title="<?php _ex( 'Qty', 'Product quantity input tooltip', 'woocommerce' ) ?>" class="input-text qty text" size="4" /></div>
